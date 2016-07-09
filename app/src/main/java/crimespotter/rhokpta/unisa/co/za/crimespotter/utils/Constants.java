140a4f36b709e3a9744839318476f755663503a4
kgundula <kgundula@gmail.com>
2016-07-09 18:46:21 +0200

    Added firebase app url

---
+++ app/src/main/java/crimespotter/rhokpta/unisa/co/za/crimespotter/utils/Constants.java
@@ -0,0 +1,12 @@
+package crimespotter.rhokpta.unisa.co.za.crimespotter.utils;
+
+import crimespotter.rhokpta.unisa.co.za.crimespotter.BuildConfig;
+
+/**
+ * Created by kgundula on 16/07/09.
+ */
+public class Constants {
+
+
+    public static final String FIREBASE_URL = BuildConfig.UNIQUE_FIREBASE_ROOT_URL;
+}
